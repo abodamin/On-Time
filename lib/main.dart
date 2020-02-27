9aa8b39d9226d1f74236f4bfea4a23cd6585fcab
ABDULLAH <abodamin0@gmail.com>
2020-02-28 00:57:30 +0300

    added bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -12,7 +10,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   GoogleMapController mapController;
-
+  bool _visibility = false;
   final LatLng _center = const LatLng(24.774265, 46.738586);
 
   void _onMapCreated(GoogleMapController controller) {
@@ -21,18 +19,10 @@ class _MyAppState extends State<MyApp> {
 
   Set<Marker> _markers;
   String _randomLocations =
-        '24.86146928,46.69903575,24.88316324,46.69854317,24.93996718,46.76643857,24.6993607,46.56438094,24.69793692,46.69062082,24.63449564,46.69748834,24.79167131,46.59003239,24.80303949,46.72174665,24.63893682,46.72256229,24.7844077,46.84134829,24.67704144,46.79674636,24.64808747,46.6998683,24.6202194,46.71829173,24.73029649,46.67289744,24.82366259,46.63587723,24.93323531,46.66575324,24.80882471,46.56780604,24.71954391,46.76374092,24.62784976,46.7145642';
+      '24.86146928,46.69903575,24.88316324,46.69854317,24.93996718,46.76643857,24.6993607,46.56438094,24.69793692,46.69062082,24.63449564,46.69748834,24.79167131,46.59003239,24.80303949,46.72174665,24.63893682,46.72256229,24.7844077,46.84134829,24.67704144,46.79674636,24.64808747,46.6998683,24.6202194,46.71829173,24.73029649,46.67289744,24.82366259,46.63587723,24.93323531,46.66575324,24.80882471,46.56780604,24.71954391,46.76374092,24.62784976,46.7145642';
 
   @override
-  void initState() {
-    super.initState();
-//  WidgetsBinding.instance.addPostFrameCallback((_){
-// _markers = _generateMarkers();
-//  });
-  }
-
-  @override
-  void didChangeDependencies(){
+  void didChangeDependencies() {
     super.didChangeDependencies();
     _markers = _generateMarkers();
   }
@@ -42,7 +32,7 @@ class _MyAppState extends State<MyApp> {
     _markers = {};
     List<String> _split = _randomLocations.split(',').toList();
 
-    for (int i = 0; i < _split.length-1; i + 2) {
+    for (int i = 0; i < _split.length - 1; i + 2) {
       _lat = double.parse(_split.elementAt(i));
       _lon = double.parse(_split.elementAt(++i));
 
@@ -53,29 +43,16 @@ class _MyAppState extends State<MyApp> {
           infoWindow: InfoWindow(
             title: 'Bus Station $i',
           ),
+          onTap: () {
+            setState(() {
+              _visibility = !_visibility;
+            });
+          },
           icon: BitmapDescriptor.defaultMarker,
         ),
       );
     }
     return _markers;
-  }
-
-    void _onAddMarkerButtonPressed() {
-      _markers = {};
-      Random _random = new Random();
-      
-    setState(() {
-      _markers.add(Marker(
-        // This marker id can be anything that uniquely identifies each marker.
-        markerId: MarkerId(_random.nextInt(1000).toString()),
-        position: _center,
-        infoWindow: InfoWindow(
-          title: 'Really cool place',
-          snippet: '5 Star Rating',
-        ),
-        icon: BitmapDescriptor.defaultMarker,
-      ));
-    });
   }
 
   @override
@@ -86,16 +63,31 @@ class _MyAppState extends State<MyApp> {
           title: Text('Maps Sample App'),
           backgroundColor: Colors.green[700],
         ),
-        body: GoogleMap(
-          markers: _markers,
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _onAddMarkerButtonPressed,
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+              markers: _markers,
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 11.0,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 200,
+                child: Visibility(
+                  visible: _visibility,
+                  child: Card(
+                    child: Center(
+                      child: Text('Dadadadaddaaaa!'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
